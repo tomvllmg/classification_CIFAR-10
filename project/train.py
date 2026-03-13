e22ce47733179906f9d0c5ab80b0d99c0c64a9a3
Charles M <charlesmitteault@gmail.com>
2026-03-13 15:53:12 +0100

    Update train.py

--- project/train.py
+++ project/train.py
@@ -1,1 +1,19 @@
-ccc
+import hydra
+from omegaconf import DictConfig
+
+@hydra.main(
+  version_base=None,
+  config_path="config1",
+  config_name="config2"
+)
+def main(cfg: DictConfig):
+  print(cfg)
+  print(cfg.training.lr)
+  print(cfg.activation)
+  print(cfg.toto.name)
+  print(cfg.toto.param.channels)
+  print(cfg.name)
+  print(cfg.param.lr)
+
+if __name__ == "__main__":
+  main()
