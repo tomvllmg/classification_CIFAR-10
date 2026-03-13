ac1cdb8859cfaafbfe50eb1595bcefe653c15db9
tomvllmg <tom.villemagne04@orange.fr>
2026-03-13 14:51:17 +0100

    Update cnn.py

--- project/model/cnn.py
+++ project/model/cnn.py
@@ -1,1 +1,22 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Subset
+import numpy as np
+import matplotlib.pyplot as plt
+import copy
 
+class CNNClassif(nn.Module):
+    def __init__(self, input_size_linear, num_channels1=16, num_channels2=32, num_classes=10):
+        super(CNNClassif, self).__init__()
+        self.cnn_layer1 = nn.Sequential(nn.Conv2d(1, num_channels1, kernel_size=5, padding=2), nn.ReLU(), nn.MaxPool2d(kernel_size=2))
+        self.cnn_layer2 = nn.Sequential(nn.Conv2d(num_channels1, num_channels2, kernel_size=5, padding=2), nn.ReLU(), nn.MaxPool2d(kernel_size=2))
+        self.linear_layer = nn.Linear(input_size_linear, num_classes)
+
+    def forward(self, x):
+        y = self.cnn_layer1(x)
+        y = self.cnn_layer2(y)
+        y = y.reshape(y.shape[0], -1)
+        out = self.linear_layer(y)
+
+        return out
