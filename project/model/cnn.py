0a502a9a881bfb1bdb85ec9ebd832d7206f3e234
tomvllmg <tom.villemagne04@orange.fr>
2026-03-13 16:09:23 +0100

    Update cnn.py

--- project/model/cnn.py
+++ project/model/cnn.py
@@ -12,13 +12,13 @@ class CNNClassif(nn.Module):
     def __init__(self, input_size_linear, nb_hidden_layers, num_channels1=16, num_channels2=32, num_classes=10):
         
         super(CNNClassif, self).__init__()
-        
+        self.nb_hidden_layers
         self.cnn_layer1 = nn.Sequential(nn.Conv2d(3, num_channels1, kernel_size=5, padding=2), nn.ReLU(), nn.MaxPool2d(kernel_size=2))
         
         out_nb_channels = num_channels1 
         self.hidden_layers = []
         
-        for i in range(1,nb_hidden_layers)
+        for i in range(1, self.nb_hidden_layers)
             layers = nn.Sequential(nn.Conv2d(out_nb_channels, out_nb_channels * 2, kernel_size=5, padding=2), nn.ReLU(), nn.MaxPool2d(kernel_size=2))
             self.hidden_layers.append(layers)
             out_nb_channels = out_nb_channels * 2
@@ -27,8 +27,8 @@ class CNNClassif(nn.Module):
 
     def forward(self, x):
         y = self.cnn_layer1(x)
-        y = self.cnn_layer2(y)
+        for i in range(1, self.nb_hidden_layers):
+            y = self.hidden_layers[i](y)    
         y = y.reshape(y.shape[0], -1)
         out = self.linear_layer(y)
-
         return out
