bd71897a43673f2f86d36a2e82c0325263137522
Charles M <charlesmitteault@gmail.com>
2026-03-16 09:24:44 +0100

    Implement build_loss function for loss selection

--- project/losses/build_loss.py
+++ project/losses/build_loss.py
@@ -1,1 +1,18 @@
+#project/losses/build_loss.py
 
+import torch.nn as nn
+
+def build_loss(cfg_loss):
+    """
+    Construit la fonction de perte.
+    """
+    name = cfg_loss.get("name", "cross_entropy").lower()
+
+    if name == "cross_entropy":
+        return nn.CrossEntropyLoss()
+        
+    elif name == "mse":
+        return nn.MSELoss()
+        
+    else:
+        raise ValueError(f"La fonction de perte '{name}' n'est pas prise en charge.")
