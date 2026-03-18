2409e9459c31eb03b72979f114c9aae871d7f8ff
tomvllmg <tom.villemagne04@orange.fr>
2026-03-18 14:26:02 +0100

    Update build_optimizer.py

--- project/optimizers/build_optimizer.py
+++ project/optimizers/build_optimizer.py
@@ -1,23 +1,13 @@
-#project/optimizers/build_optimizer.py
+# project/optimizers/build_optimizer.py
 
-import torch.optim as optim
+from hydra.utils import instantiate
 
 def build_optimizer(cfg_optimizer, model):
     """
-    Construit l'optimiseur PyTorch en fonction de la configuration YAML.
+    Construit l'optimiseur en utilisant la magie de Hydra (_target_).
     """
-    # On récupère le nom (ex: "adam" ou "sgd") défini dans le YAML
-    name = cfg_optimizer.get("name", "adam").lower()
-    lr = cfg_optimizer.get("lr", 0.001)
-    weight_decay = cfg_optimizer.get("weight_decay", 0.0)
-
-    if name == "adam":
-        # On passe les paramètres du modèle à l'optimiseur
-        return optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
-        
-    elif name == "sgd":
-        momentum = cfg_optimizer.get("momentum", 0.9)
-        return optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
-        
-    else:
-        raise ValueError(f"L'optimiseur '{name}' n'est pas pris en charge.")
+    # Hydra lit `_target_: torch.optim.Adam` dans ton YAML 
+    # et crée l'optimiseur en lui passant les paramètres du modèle !
+    optimizer = instantiate(cfg_optimizer.param, params=model.parameters())
+    
+    return optimizer
