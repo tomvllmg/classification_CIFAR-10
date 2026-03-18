75b4fa37500ee0fa58439dcd7268d8e8efd2da63
tomvllmg <tom.villemagne04@orange.fr>
2026-03-18 16:24:46 +0100

    Update train.py

--- project/train.py
+++ project/train.py
@@ -9,7 +9,7 @@ from project.data.dataset import get_dataloaders
 from project.losses.build_loss import build_loss
 from project.optimizers.build_optimizer import build_optimizer
 from project.schedulers.build_schedulers import build_scheduler
-from project.utils.training_loop import train_model
+from project.utils.early_stopping import train_model
 
 @hydra.main(
   version_base=None,
@@ -21,38 +21,31 @@ def main(cfg: DictConfig):
   Fonction principale exécutée lors du lancement de train.py
   'cfg' contient toute la configuration assemblée depuis vos fichiers YAML.
   """
-   # 1. Configuration du matériel (Utiliser la carte graphique si possible)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(f"Entraînement lancé sur : {device}")
+   
+  # 2. Préparation des données
+  # On passe la partie "data" de la configuration (taille des batchs, etc.)
+  train_loader, val_loader = get_dataloaders(cfg.data)
 
-    # 2. Préparation des données
-    # On passe la partie "data" de la configuration (taille des batchs, etc.)
-    train_loader, val_loader = get_dataloaders(cfg.data)
+  # 3. Création du modèle et envoi sur le bon appareil (CPU ou GPU)
+  model = CNNClassif(cfg.model).to(device)
 
-    # 3. Création du modèle et envoi sur le bon appareil (CPU ou GPU)
-    model = CNNClassif(cfg.model).to(device)
+  # 4. Assemblage via vos "Builders"
+  # On utilise les fonctions que nous avons vues précédemment
+  criterion = build_loss(cfg.loss)
+  optimizer = build_optimizer(cfg.optimizer, model)
+  scheduler = build_scheduler(cfg.scheduler, optimizer)
 
-    # 4. Assemblage via vos "Builders"
-    # On utilise les fonctions que nous avons vues précédemment
-    criterion = build_loss(cfg.loss)
-    optimizer = build_optimizer(cfg.optimizer, model)
-    scheduler = build_scheduler(cfg.scheduler, optimizer)
-
-    # 5. Lancement du moteur d'entraînement
-    print("Début de la boucle d'entraînement...")
-    train_model(
-        model=model,
-        train_loader=train_loader,
-        val_loader=val_loader,
-        criterion=criterion,
-        optimizer=optimizer,
-        scheduler=scheduler,
-        epochs=cfg.epochs,
-        device=device
-    )
-    
-    print("Entraînement terminé !")
-
+  # 5. Boucle d'entraînement
+  train_model(
+      model=model,
+      train_loader=train_loader,
+      val_loader=val_loader,
+      criterion=criterion,
+      optimizer=optimizer,
+      scheduler=scheduler,
+      epochs=cfg.epochs,
+      device=device
+  )
 
 if __name__ == "__main__":
   main()
