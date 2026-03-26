e8d8b4b984cdf893e8774e8c0c78b878c5567436
Charles M <charlesmitteault@gmail.com>
2026-03-26 09:04:50 +0100

    Update dataloader.py

--- project/data/dataloader.py
+++ project/data/dataloader.py
@@ -4,8 +4,8 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader, Subset
 
 def build_dataloaders(cfg_data, cfg_aug):
-    # Tes valeurs exactes de normalisation !
-    normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616))
+    # Valeurs de normalisation
+    normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)) #moyennes et écarts-types de chaque canal calculées sur l'ensemble du dataset CIFAR-10
     
     val_transform = transforms.Compose([transforms.ToTensor(), normalize])
     
@@ -19,12 +19,12 @@ def build_dataloaders(cfg_data, cfg_aug):
     else:
         train_transform = val_transform # Pas d'augmentation
 
-    # 1. Chargement des données brutes
+    # Chargement des données brutes
     dataset_train_aug = torchvision.datasets.CIFAR10(root=cfg_data.data_dir, train=True, download=True, transform=train_transform)
     dataset_val_clean = torchvision.datasets.CIFAR10(root=cfg_data.data_dir, train=True, download=True, transform=val_transform)
     dataset_test = torchvision.datasets.CIFAR10(root=cfg_data.data_dir, train=False, download=True, transform=val_transform)
 
-    # 2. Le Split 80/20
+    # Split 80/20 du dataset
     num_train = len(dataset_train_aug)
     split = int(0.8 * num_train)
     
@@ -32,23 +32,21 @@ def build_dataloaders(cfg_data, cfg_aug):
     indices = torch.randperm(num_train).tolist()
     train_idx, valid_idx = indices[:split], indices[split:]
     
-    # ==========================================
-    # L'ASTUCE POUR LE CPU (Fast Dev Run)
-    # ==========================================
+    # Choix des indices des dataset d'entrainement et de validation pour réduire le temps de calcul
     if cfg_data.get("debug_mode", False):
-        # On ne garde que les 800 premiers du train, 200 du valid
+        # On ne garde que les 40000 premiers du train, 10000 du valid
         train_idx = train_idx[:40000]
         valid_idx = valid_idx[:10000]
         
-        # Et on bride le test set à 100
-        test_indices = torch.arange(100).tolist()
+        # Et on bride le test set à 5000
+        test_indices = torch.arange(5000).tolist()
         dataset_test = Subset(dataset_test, test_indices)
 
-    # 3. Création des subsets pour Train et Valid
+    # Création des subsets pour Train et Valid
     train_subset = Subset(dataset_train_aug, train_idx)
     valid_subset = Subset(dataset_val_clean, valid_idx)
 
-    # 4. DataLoaders
+    # DataLoaders
     train_loader = DataLoader(train_subset, batch_size=cfg_data.batch_size, shuffle=True)
     valid_loader = DataLoader(valid_subset, batch_size=cfg_data.batch_size, shuffle=False)
     test_loader = DataLoader(dataset_test, batch_size=cfg_data.batch_size, shuffle=False)
