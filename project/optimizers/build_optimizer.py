f99787fa6a8fb2c39abdb2a8efbb36a8f449c1e0
Charles M <charlesmitteault@gmail.com>
2026-03-26 09:30:10 +0100

    Update build_optimizer.py

--- project/optimizers/build_optimizer.py
+++ project/optimizers/build_optimizer.py
@@ -6,9 +6,8 @@ def build_optimizer(cfg_optimizer, model):
     Sortie : Optimiseur
     Fonction : Construit l'optimiseur en utilisant Hydra avec _target_.
     """
-    # 
-    # Hydra lit `_target_: torch.optim.Adam` dans ton YAML 
-    # crée l'optimiseur en lui passant les paramètres du modèle
+    # Hydra exploite le champ _target_ du fichier YAML pour instancier dynamiquement l'optimiseur (ici torch.optim.Adam)
+    # en lui injectant les paramètres du modèle ainsi que les hyperparamètres de configuration.
     optimizer = instantiate(cfg_optimizer.param, params=model.parameters())
     
     return optimizer
