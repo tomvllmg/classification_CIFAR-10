c9d6ae7da0487c629a8ec00f1f46c82d80702973
tomvllmg <tom.villemagne04@orange.fr>
2026-03-26 09:37:52 +0100

    Update build_schedulers.py

--- project/schedulers/build_schedulers.py
+++ project/schedulers/build_schedulers.py
@@ -2,11 +2,10 @@ from hydra.utils import instantiate
 
 def build_scheduler(cfg_scheduler, optimizer):
     """
-    Entrées : 
-    Sortie :
+    Entrées : Dictionnaire cfg_scheduler, Optimiseur
+    Sortie : Scheduler
     Fonction : Construit le scheduler de taux d'apprentissage.
     """
-    # Bonne pratique : permettre de ne pas utiliser de scheduler
     if cfg_scheduler is None or cfg_scheduler.get("name") == "none":
         return None
 
