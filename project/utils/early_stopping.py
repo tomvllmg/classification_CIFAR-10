b4a2d75d01006d0ac56d453e42928d85c99a28ff
Charles M <charlesmitteault@gmail.com>
2026-03-26 10:40:44 +0100

    Update early_stopping.py

--- project/utils/early_stopping.py
+++ project/utils/early_stopping.py
@@ -1,21 +1,15 @@
-# Attention  il faut split le train_data en  Define the validation set by splitting the training data into 2 subsets (80% training and 20% validation)
-# ==============
-# attention peut etre gerer le device ici 
-
 import torch
 import copy
 import wandb
 
-# Eval function
-
 def eval_cnn_classifier(model, eval_dataloader):
 
-    # Set the model in evaluation mode
+    # On passe en mode évaluation
     model.eval()
 
-    # In test phase, we don't need to compute gradients (for memory efficiency)
+    # Dans la phase de test on ne calcule pas les gradients pour la mémoire
     with torch.no_grad():
-        # initialize the total and correct number of labels to compute the accuracy
+        # Initialise le nombre total et correct de labels pour calculer la précision
         correct = 0
         total = 0
         for images, labels in eval_dataloader:
@@ -27,15 +21,14 @@ def eval_cnn_classifier(model, eval_dataloader):
     accuracy = 100 * correct / total
 
     return accuracy
-
-# Training with vlidation
+    
 
 def train_val_classifier(model_tr, train_dataloader, valid_dataloader, num_epochs, loss_fn, optimizer, scheduler, patience, verbose=True):
 
-    # Initialize a list for storing the training loss over epochs
+    # Liste pour stocker les pertes au cours de epochs
     train_losses = []
 
-    # EARLY STOPPING
+    # Early stopping
     best_acc = 0
     best_model = None
     list_acc =[]
@@ -45,13 +38,11 @@ def train_val_classifier(model_tr, train_dataloader, valid_dataloader, num_epoch
     for epoch in range(num_epochs):
         
         model_tr.train()
-        # Initialize the training loss for the current epoch
+        # Initialise la perte
         tr_loss = 0
 
-        # Iterate over batches using the dataloader
+        # Iteration les batches
         for batch_index, (images, labels) in enumerate(train_dataloader):
-
-            # Pareil ici, on ne reshape pas
             pred_labels = model_tr(images)
             loss = loss_fn(pred_labels, labels)
 
@@ -59,19 +50,16 @@ def train_val_classifier(model_tr, train_dataloader, valid_dataloader, num_epoch
             loss.backward()
             optimizer.step()
 
-            # Update the current epoch loss
-            # Note that 'loss.item()' is the loss averaged over the batch, so multiply it with the current batch size to get the total batch loss
             tr_loss += loss.item() * images.shape[0]
 
-        # At the end of each epoch, get the average training loss and store it
+        # A la fin de chaque epoch, on stock la perte moyenne d'entrainement
         tr_loss = tr_loss/len(train_dataloader.dataset)
         train_losses.append(tr_loss)
 
-        # Display the training loss
+        # Affichage
         if verbose:
             print('Epoch [{}/{}], Training loss: {:.4f}'.format(epoch+1, num_epochs, tr_loss))
 
-        # LAB 4.1
         accuracy = eval_cnn_classifier(model_tr, valid_dataloader)
         list_acc.append(accuracy)
 
@@ -91,7 +79,7 @@ def train_val_classifier(model_tr, train_dataloader, valid_dataloader, num_epoch
         else:
             epochs_without_improvement += 1 # Pas de progrès, on incrémente
 
-        # CONDITION D'ARRÊT PRÉCOCE
+        # Condition d'arret précoce
         if epochs_without_improvement >= patience:
             if verbose:
                 print(f"Early stopping déclenché à l'époque {epoch+1}")
